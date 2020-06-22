13fece732a9a8958d7c73ca1a0895b21e55e979f
jair <jacobo_9410@hotmail.com>
2020-06-21 23:15:32 -0500

    fix mode history

--- src/app/router.js
+++ src/app/router.js
@@ -1,56 +1,55 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Demo from './views/Demo.vue'
-import Documentacion from './views/Documentacion.vue'
-import NotFoundComponent from './views/NotFoundComponent.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Demo from "./views/Demo.vue";
+import Documentacion from "./views/Documentacion.vue";
+import NotFoundComponent from "./views/NotFoundComponent.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/documentacion',
-      alias: '/documentación',
-      name: 'documentación',
+      path: "/documentacion",
+      alias: "/documentación",
+      name: "documentación",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: Documentacion
-    }, 
+      component: Documentacion,
+    },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: About
+      component: About,
     },
-   
+
     {
-      path: '/demo/:id',
-      name: 'demoIma',
+      path: "/demo/:id",
+      name: "demoIma",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: Demo
+      component: Demo,
     },
     {
-      path: '/demo',
-      name: 'demo',
+      path: "/demo",
+      name: "demo",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: Demo
+      component: Demo,
     },
-    { path: '*', component: NotFoundComponent }
-  ]
-})
+    { path: "*", component: NotFoundComponent },
+  ],
+});
